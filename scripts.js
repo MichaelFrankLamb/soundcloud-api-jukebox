01f496b662d3d8d904c1bac2fc61967e75e96ec1
Michael Lamb <mflambjr@gmail.com>
2018-04-05 23:16:51 -0400

    Merge conflict from last commit, uncommented Vanilla JS

--- scripts.js
+++ scripts.js
@@ -47,54 +47,54 @@ function playNext() {
 // Functionality from original jukebox page
 
 
-// var music = this;
-// music.audio = document.createElement('audio');
-// music.songTitle = document.getElementById('songTitle');
-// music.nextSongTitle = document.getElementById('nextSongTitle');
-// var next = document.getElementById('next');
-// var prev = document.getElementById('prev');
-// var playPause = document.getElementById('playPause');
-// var stop = document.getElementById('stop'); 
+var music = this;
+music.audio = document.createElement('audio');
+music.songTitle = document.getElementById('songTitle');
+music.nextSongTitle = document.getElementById('nextSongTitle');
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+var playPause = document.getElementById('playPause');
+var stop = document.getElementById('stop'); 
 
 
 
-// music.loadSong = loadSong;
-//     function loadSong(){
-//     music.audio.src = "songs/" + music.songs[music.currentSong];
-//     music.songTitle.textContent = (music.currentSong + 1) + ". " + music.songs[music.currentSong]; 
-//     music.nextSongTitle.innerHTML = "<b>Next Song: </b>" + music.songs[(music.currentSong + 1) % +(music.songs.length)];
-//     music.audio.play();
-//     }
+music.loadSong = loadSong;
+    function loadSong(){
+    music.audio.src = "songs/" + music.songs[music.currentSong];
+    music.songTitle.textContent = (music.currentSong + 1) + ". " + music.songs[music.currentSong]; 
+    music.nextSongTitle.innerHTML = "<b>Next Song: </b>" + music.songs[(music.currentSong + 1) % +(music.songs.length)];
+    music.audio.play();
+    }
 
-//     playPause.addEventListener("click",function playPause(e) {
-//         if(music.audio.paused) {
-//             music.audio.play();
-//             e.target.src = "images/pause.png";
-//         }else{
-//             music.audio.pause();
-//             e.target.src = "images/play.png";
-//         }
-//     } );
-// next.addEventListener("click",function(e) {
-//     ++music.currentSong;
-//     music.currentSong = (music.currentSong > 2) ? music.songs.length - 3 : music.currentSong;
-//     loadSong();
-// });
+    playPause.addEventListener("click",function playPause(e) {
+        if(music.audio.paused) {
+            music.audio.play();
+            e.target.src = "images/pause.png";
+        }else{
+            music.audio.pause();
+            e.target.src = "images/play.png";
+        }
+    } );
+next.addEventListener("click",function(e) {
+    ++music.currentSong;
+    music.currentSong = (music.currentSong > 2) ? music.songs.length - 3 : music.currentSong;
+    loadSong();
+});
 
-// prev.addEventListener("click",function(e) {
-//     music.currentSong--;
-//     music.currentSong = (music.currentSong < 0) ? music.songs.length - 1 : music.currentSong;
-//     loadSong();
-// });
+prev.addEventListener("click",function(e) {
+    music.currentSong--;
+    music.currentSong = (music.currentSong < 0) ? music.songs.length - 1 : music.currentSong;
+    loadSong();
+});
 
-// stop.addEventListener("click",function(e) {  
-//     music.audio.pause();
-//     music.audio.load();
-//     music.audio.src = ""; 
-//     music.audio.src = "songs/" + music.songs[music.currentSong];                  
-// });                                           
-// }
+stop.addEventListener("click",function(e) {  
+    music.audio.pause();
+    music.audio.load();
+    music.audio.src = ""; 
+    music.audio.src = "songs/" + music.songs[music.currentSong];                  
+});                                           
 
-// var myJukebox = new Jukebox()
 
-// window.onload = myJukebox.loadSong;
+var myJukebox = new Jukebox()
+
+window.onload = myJukebox.loadSong;
